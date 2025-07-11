ff4fa5eeb3114a36d12282d3030148feb910fcb6
Deadlyzangzoo <deadlyzangzoo@outlook.com>
2025-07-11 13:38:05 +0100

    Did some more file management setup, made sure my IDE was working with Unity

---
+++ cyberpunk game/Assets/Scripts/hello world.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log("And hello to anyone who can see this code, as well.");
+        Debug.Log("I hope you enjoy this game jam.");
+    }
+}
